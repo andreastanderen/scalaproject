5bbdcf7c5604645369724ea745b04eadf27dfe06
andreastanderen <andrea_standeren@hotmail.com>
2019-10-31 13:37:51 +0100

    fix status SUCCESS

--- src/main/scala/Transaction.scala
+++ src/main/scala/Transaction.scala
@@ -63,13 +63,20 @@ class Transaction(val transactionsQueue: TransactionQueue,
 
       def doTransaction() = {
         this.synchronized{
-        val res_from = from.withdraw(amount)
-        val res_to = to.deposit(amount)
+          this.attempt += 1
+          val res_from = from.withdraw(amount)
+          if (res_from.isLeft){
+            val res_to = to.deposit(amount)
+            this.status = TransactionStatus.SUCCESS
+            t = this.transactionsQueue.pop
+            this.processedTransactions.push(t)
+          }
+
         }
       }
       // TODO - project task 3
       // make the code below thread safe
-      if (status == TransactionStatus.PENDING) {
+      if (this.status == TransactionStatus.PENDING && this.attempt < this.allowedAttemps) {
         doTransaction
         Thread.sleep(50) // you might want this to make more room for
         // new transactions to be added to the queue
