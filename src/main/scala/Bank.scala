4f6920a87bf3667944315925eb00332dc14f33fe
theodorc <theodor.rene@gmail.com>
2019-10-31 13:19:49 +0100

    Add nonworking concurrecny to processTransaction

--- src/main/scala/Bank.scala
+++ src/main/scala/Bank.scala
@@ -2,13 +2,12 @@ class Bank(val allowedAttempts: Integer = 3) {
 
     private val transactionsQueue: TransactionQueue = new TransactionQueue()
     private val processedTransactions: TransactionQueue = new TransactionQueue()
-    val threads: Thread =  
 
 
     def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
       val transaction = new Transaction(transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
       transactionsQueue.push(transaction)
-      processTransactions
+      Main.thread(processTransactions)
     }
                                                 // TODO
                                                 // project task 2
@@ -18,7 +17,14 @@ class Bank(val allowedAttempts: Integer = 3) {
     private def processTransactions: Unit = {
 	  while (!this.transactionsQueue.isEmpty){
           val transaction = this.transactionsQueue.pop
-          val t = new Thread(transaction).start
+          val t:Thread = new Thread(transaction)
+          t.start()
+          t.join()
+          if (transaction.status == TransactionStatus.PENDING){
+            this.transactionsQueue.push(transaction)
+          } else{
+            this.processedTransactions.push(transaction)
+          }
       }
     }
                                                 // TOO
